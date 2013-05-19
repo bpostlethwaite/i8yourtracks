9c8abcfd3369a023f933f3f286a5c07447b67a1d
Ben Postlethwaite <post.ben.here@gmail.com>
2013-05-18 18:45:31 -0700

    download button

--- test/getmix.js
+++ test/getmix.js
@@ -2,7 +2,8 @@ var APIKEY = "fd44d5cd660dafd46ea7f989acba667720c17df3"
 var emitMix = require('./../lib/mixEmitter')(APIKEY)
 
 emitMix.on('mix', function (mix) {
-  //console.log(mix)
+  console.log(dateString(mix.date))
+
 })
 
 emitMix.on('error', function (data) {
@@ -12,3 +13,12 @@ emitMix.on('error', function (data) {
 })
 
 emitMix.start('rustyspoons')
+
+
+function dateString (dateObj) {
+  var month = dateObj.getUTCMonth()
+  var day = dateObj.getUTCDate()
+  var year = dateObj.getUTCFullYear()
+
+  return year + "/" + month + "/" + day
+}
